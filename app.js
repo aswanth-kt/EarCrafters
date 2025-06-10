59a1679b91de8590dfad4c95c02ebd097c0484c9
aswanthkt31 <aswanthkt99@gmail.com>
2025-06-10 12:59:01 +0530

    Google auth fix error

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({extended: true}));    //convert urls string
 app.use(session({
     secret : process.env.SESSION_SECRET,
     resave : false,
-    saveUninitialized : true,
+    saveUninitialized : false,
     store: MongoStore.create({mongoUrl: process.env.MONGODB_URI}),
     cookie : {
         secure : false, //Remind change to true at production time
