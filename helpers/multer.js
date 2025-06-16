5769c96f6b1d86f5d79a74009323bdb90cdc6dfd
aswanthkt31 <aswanthkt99@gmail.com>
2025-06-16 21:02:27 +0530

    Fix address form validation and image validation in multer

--- helpers/multer.js
+++ helpers/multer.js
@@ -21,4 +21,13 @@ const storage = multer.diskStorage({
     },
 });
 
+// File filter
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image/")) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only image files are allowed!"), false);
+  }
+};
+
 module.exports = storage;
